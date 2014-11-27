906cb8537e77afd7c0073ad79ec7c4a716e764da
1100447 <1100447@isep.ipp.pt>
2014-11-27 16:26:07 +0000

    FARST!

---
+++ idei/DAL/IDEIContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using idei.Models;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace idei.DAL
+{
+    public class IDEIContext : DbContext
+    {
+        public IDEIContext() : base("IDEIContext") { }
+        public DbSet<Album> Albums { get; set; }
+        public DbSet<Genre> Genres { get; set; }
+        public DbSet<Artist> Artists { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
