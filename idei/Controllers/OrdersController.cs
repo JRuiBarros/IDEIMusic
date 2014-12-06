3b65343f977c98fbd6b4f2089d899225446aec09
1100447 <1100447@isep.ipp.pt>
2014-12-06 14:23:45 +0000

    ajax a nao dar

--- idei/Controllers/OrdersController.cs
+++ idei/Controllers/OrdersController.cs
@@ -145,7 +145,36 @@ namespace idei.Controllers
             db.SaveChanges();
             return RedirectToAction("Index");
         }
+        // AJAX:
+        [HttpPost]
+        public ActionResult RemoveFromOrder(int orderListId)
+        {
+            // Remove the item from the cart
+            //var cart = ShoppingCart.GetCart(this.HttpContext);
 
+            // Get the name of the album to display confirmation
+            //string albumName = storeDB.Carts
+            //    .Single(item => item.RecordId == id).Album.Title;
+
+            //// Remove from cart
+            //int itemCount = cart.RemoveFromCart(id);
+
+            //// Display the confirmation message
+            //var results = new ShoppingCartRemoveViewModel
+            //{
+            //    Message = Server.HtmlEncode(albumName) +
+            //        " has been removed from your shopping cart.",
+            //    CartTotal = cart.GetTotal(),
+            //    CartCount = cart.GetCount(),
+            //    ItemCount = itemCount,
+            //    DeleteId = id
+            //};
+            //return Json(results);
+            var orderList = db.OrderLists.Find(orderListId);
+            db.OrderLists.Remove(orderList);
+            db.SaveChanges();
+            return null;
+        }
         protected override void Dispose(bool disposing)
         {
             if (disposing)
